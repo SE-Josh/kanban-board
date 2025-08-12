24e5332101c00d5bc4c046e3f25d77110cb61b21
Josh <jokezxcv@gmail.com>
2025-08-12 11:16:12 +0800

    chore: 開放 /out 資料夾上 deploy 分支

---
+++ out/_next/static/chunks/app/layout-2eac4b1b54116a47.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{2564:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,9324,23)),Promise.resolve().then(n.bind(n,6671))},9324:()=>{}},e=>{e.O(0,[533,671,441,964,358],()=>e(e.s=2564)),_N_E=e.O()}]);
